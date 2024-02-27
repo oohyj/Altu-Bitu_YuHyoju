b4efc42d1cac92d4779eb2ae5a2208aed3726876
oohyj <hyj0660@gmail.com>
2024-02-27 09:45:24 +0100

    [스택,큐,덱] 2월 27일

---
+++ 02_스택_큐_덱/필수/1158.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+
+    int n , k;
+    cin >> n>>k ;
+
+    queue <int> q ;
+
+    for (int i =1; i<=n ; i++){
+        q.push(i);
+    }
+
+    cout << "<";
+
+    while(!q.empty()){
+    for(int i =1; i<k; i++){
+        q.push(q.front());
+        q.pop();
+    }
+    if (q.size()==1) cout << q.front() << ">";
+    else  cout << q.front() << ", ";
+    q.pop();
+    }
+
+    return 0;
+
+}
