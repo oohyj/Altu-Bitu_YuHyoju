89c0a2584565cf6a27810f43ce3f06d3f0c3712f
oohyj <hyj0660@gmail.com>
2024-05-10 18:09:29 +0900

    [이분탐색] 5월 10일 - 추가제출

--- 10_이분탐색/필수/16401.cpp
+++ 10_이분탐색/필수/16401.cpp
@@ -1,74 +1,45 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int findMaxLength(int m , int n , vector <int> snack){
-    int max = 1; // 과자의 최대 길이
-    int cnt = 0; // 과자 개수
-    int a ;
-    // 조카 수가 과자 개수보다 적은 경우
-    if( n >= m ){
-        a = n; //뒤에서부터 최대 길이 찾기
-        max = snack[a - 1];
+#define MAX_COOKIE 1000000000 //쿠키의 최대 길이
 
-        while(max > 0 ) { // 줄 수 있는 과자 개수가 조카 수보다 작은 경우에만 while문 돌기
+int binarySearch(int m , vector<int> &cookie){
+    int right = MAX_COOKIE; //과자의 최대 길이
+    int left  = 1; //쿠키의 최소 길이
+    int res = 0; //막대 과자의 최대 길이
 
-            for (int i = 0; i <= n; i++) {
-                if (max <= snack[i]) { // 만약 과자 길이가 max 값보다 크면
-                    cnt++; // 줄 수 있는 과자 개수가 늘어남
-                }
-            }
-            if (cnt < m) { // 줄 수 있는 과자 개수가 조카 수보다 적으면
-                a--;
-                max = snack[a-1];
-                cnt = 0;
-            }
-            else if(cnt == m){
-                break;
-            }
+    while(left <= right){
+        int mid = (left + right) / 2;
+        int cnt = 0; // 해당 길이로 몇 명에게 나눠줄 수 과자 개수
+        //mid 길이만큼 나눠주면 몇 명에게 나눠줄 수 있는지 세기
+        for(int i =0; i< cookie.size(); i++){ //주어진 과자를 모두 탐색
+            cnt += cookie[i]/mid; // 주어진 과자 길이로 나올 수 있는 과자 개수 세기
         }
+        if(cnt >= m){ // 만약 조카 수보다 과자 개수가 크다면
+            left = mid +1; // 과자 길이를 더 크게 해서 탐색하기
+            res = mid; // mid 길이로는 일단 조카의 수를 만족하니까 res를 mid로
+        }
+        else {//mid 길이로 나눠줄 수 있는 과자의 개수가 조카 수보다 작다면
+            right = mid - 1; //과자 길이를 더 작게해서 다시
+        }
+
     }
-    else{ // 조카 수가 과자 개수보다 많은 경우
-       a = n;
-       max = snack[a-1]; // 가지고 있는 과자 개수 중에 가장 큰 값
-       while(max > 0){ // 조카 수보다 과자 개수가 적은 상태에서
-           for(int i =0; i <n ; i++){
-               cnt += snack[i] / max ; // 하나에
-           }
-           if(cnt < m){ // 만약 만들 수 있는 과자 개수가 조카 수보다 작다면
-               if((a-2) < 0){ //근데 a가 0보다 작아지게 되면
-                   max = max-1; //max 값을 1씩 줄이기
-                   cnt = 0;
-               }
-               else {
-                   a--; // 더 작은 값을 max 값으로 만들기
-                   max = snack[a - 1];
-                   cnt = 0;// 개수 초기화
-               }
-           }
-           else if(cnt == m){
-               break;
-           }
-       }
-    }
-    return max;
+    return res; //나눠줄 수 있는 최대 과자 길이
 }
 
 int main(){
-
-    int m , n , len;
-    vector<int> snack;
-
+    int m , n; // m: 조카 수 , n : 과자 개수
+    //입력
     cin >> m >> n;
-    for(int i =0; i<n; i++){
-        cin >> len ;
-        snack.push_back(len);
+    vector<int> cookie(n); //주어진 과자 길이 담을 배열
+
+    for(int i = 0; i< n; i++){
+        cin >> cookie[i];
     }
+    //이분 탐색
+    cout << binarySearch(m , cookie);
 
-    sort(snack.begin() , snack.end());
-
-    cout << findMaxLength(m , n , snack) <<'\n';
     return 0;
 }
